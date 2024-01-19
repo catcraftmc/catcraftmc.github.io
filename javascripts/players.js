1fcfac3cf3fcf9a624d8dbdf549cbb2e53899f81
Серёжа <szarkan@vk.com>
2024-01-19 12:37:49 +0300

    Deployed 0cac0b5 with MkDocs version: 1.5.3

--- javascripts/players.js
+++ javascripts/players.js
@@ -1,23 +1,49 @@
-async function updatePlayerCount() {
-    const serverAddress = '107.161.154.192:25565'; // Replace with your server IP
-    const apiUrl = `https://api.mcsrvstat.us/2/${serverAddress}`;
-    
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        
-        if (data.online) {
-            document.getElementById('player-count').innerText = data.players.online;
-        } else {
-            document.getElementById('player-count').innerText = 'Server Offline';
+async function getMinecraftServerData(retryCount = 3) {
+    const serverURL = 'https://apis.bithole.dev/mc/ping-server?host=play.catcraftmc.ru&port=25565';
+
+    while (retryCount > 0) {
+        try {
+            const response = await fetch(serverURL);
+
+            if (!response.ok) {
+                throw new Error(`Сервер ответил: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log("Данные сервера:", data);
+
+            if (data.players.online) {
+                if (data.players.online == 1) {
+                    document.getElementById('player-count').innerText = `${data.players.online} игрок`;
+                }
+                else if (data.players.online > 1 && data.players.online < 5) {
+                    document.getElementById('player-count').innerText = `${data.players.online} игрока`;
+                }
+                else if (data.players.online >= 5) {
+                    document.getElementById('player-count').innerText = `${data.players.online} игроков`;
+                }
+                    
+            } else {
+                document.getElementById('player-count').innerText = 'Сервер выключен =(';
+            }
+
+            break; // Break the loop if the request was successful
+        } catch (error) {
+            if (error == TypeError){
+                return;
+            }
+            console.error("Не смог взять данные:", error);
+            retryCount--;
+            if (retryCount === 0) {
+                document.getElementById('player-count').innerText = "Сервер не отвечает =(";
+                break;
+            }
+            console.log(`Пробую... Осталось попыток: ${retryCount}`);
+            await new Promise(resolve => setTimeout(resolve, 3000)); // Wait for 3 seconds before retrying
         }
-    } catch (error) {
-        console.error('Error fetching server data:', error);
-        document.getElementById('player-count').innerText = 'Error fetching data';
     }
 }
 
-// Call updatePlayerCount when the page content is fully loaded
-document.addEventListener('DOMContentLoaded', (event) => {
-    updatePlayerCount();
-});
+getMinecraftServerData();
+
+
